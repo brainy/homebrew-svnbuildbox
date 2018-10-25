d743027c15f960dc0c095e9dc9c96581656adcd1
Branko Čibej <brane@apache.org>
2018-10-25 12:09:01 +0200

    New formula: APR-Util without crypto module.

---
+++ Formula/apr-util-nocrypto.rb
@@ -0,0 +1,30 @@
+class AprUtilNocrypto < Formula
+  desc "Companion library to apr, the Apache Portable Runtime library"
+  homepage "https://apr.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=apr/apr-util-1.6.1.tar.bz2"
+  sha256 "d3e12f7b6ad12687572a3a39475545a072608f4ba03a6ce8a3778f607dd0035b"
+
+  keg_only :versioned_formula, "Bla bla"
+
+  depends_on "apr"
+
+  def install
+    # Install in libexec otherwise it pollutes lib with a .exp file.
+    system "./configure", "--prefix=#{libexec}",
+                          "--with-apr=#{Formula["apr"].opt_prefix}",
+                          "--without-crypto"
+    system "make"
+    system "make", "install"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    rm Dir[libexec/"lib/*.la"]
+    rm Dir[libexec/"lib/apr-util-1/*.la"]
+
+    # No need for this to point to the versioned path.
+    inreplace libexec/"bin/apu-1-config", libexec, opt_libexec
+  end
+
+  test do
+    assert_match opt_libexec.to_s, shell_output("#{bin}/apu-1-config --prefix")
+  end
+end
